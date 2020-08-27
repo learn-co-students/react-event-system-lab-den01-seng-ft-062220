47bccfc57e8e176f78772aa88ebd6708a753afdc
Sam Evans <evansst@icloud.com>
2020-08-27 13:08:03 -0600

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,9 @@
-// Code EyesOnMe Component Here
+import React from "react";
+
+export default function EyesOnMe(props) {
+  return (
+    <button onFocus={() => console.log('Good!')} onBlur={() => console.log('Hey! Eyes on me!')} >
+
+    </button>
+  )
+}
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,9 @@
-// Code Keypad Component Here
+import React from "react";
+
+export default function Keypad(props) {
+  return (
+    <input type="password" onKeyUp={() => console.log('Entering password...')} >
+
+    </input>
+  );
+}
